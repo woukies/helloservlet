86974eab09a6568c1b1b1a7b7f0f1f3192ab319a
GreenAcademy <example@noreply.com>
2021-10-07 11:58:36 +0900

    코드 개선

--- src/test/HealthClub.java
+++ src/test/HealthClub.java
@@ -34,7 +34,7 @@ public class HealthClub {
 					int numMember = scanner.nextInt();
 					HealthMember member = null;
 					for (int i = 0; i < numMember; i++) {
-						member = new HealthMember();
+						member = new HealthMember(true);
 						System.out.println("회원 정보를 입력하세요.");
 						System.out.print("회원 아이디: ");
 						member.setId(scanner.next());
@@ -52,9 +52,9 @@ public class HealthClub {
 				menuIndex = 1;
 				break;
 			case 22:
-				System.out.print("삭제할 회원 아이디를 입력하세요 >> ");
-				String idMember = scanner.next();
-				int isDelete = manager.deleteMember(idMember);
+				System.out.print("삭제할 회원 번호를 입력하세요 >> ");
+				int numMember = scanner.nextInt();
+				int isDelete = manager.deleteMember(numMember);
 				if (isDelete == 1) {
 					System.out.println("해당 해원은 삭제되었습니다.");
 				} else if (isDelete == 0) {
@@ -132,11 +132,15 @@ class MemberManager {
 
 	public void validateTable() throws Exception {
 		Connection conn = null;
+		ResultSet rs = null;
 
 		try {
 			conn = getConnection();
 			try {
-				conn.prepareStatement("SELECT id FROM healthmember").executeQuery();
+				rs = conn.prepareStatement("SELECT MAX(num) FROM healthmember").executeQuery();
+				if (rs.next()) {
+					HealthMember.count = rs.getInt(1) + 1;
+				}
 			} catch (Exception e) {
 				conn.prepareStatement("CREATE TABLE healthmember(" + "num NUMBER(5), " + "id VARCHAR2(10), "
 						+ "name VARCHAR2(10), " + "location VARCHAR2(10))").executeUpdate();
@@ -144,6 +148,9 @@ class MemberManager {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
+			if (rs != null) {
+				rs.close();
+			}
 			if (conn != null) {
 				conn.close();
 			}
@@ -154,18 +161,12 @@ class MemberManager {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		int num = 1;
 
 		try {
 			conn = getConnection();
 
-			rs = conn.prepareStatement("SELECT MAX(num) FROM healthmember").executeQuery();
-			if (rs.next()) {
-				num = rs.getInt(1) + 1;
-			}
-
 			pstmt = conn.prepareStatement("INSERT INTO healthmember VALUES(?, ?, ?, ?)");
-			pstmt.setInt(1, num);
+			pstmt.setInt(1, member.getNum());
 			pstmt.setString(2, member.getId());
 			pstmt.setString(3, member.getName());
 			pstmt.setString(4, member.getLocation());
@@ -185,7 +186,7 @@ class MemberManager {
 		}
 	}
 
-	public int deleteMember(String id) throws Exception {
+	public int deleteMember(int num) throws Exception {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -193,7 +194,7 @@ class MemberManager {
 
 		try {
 			conn = getConnection();
-			rs = conn.createStatement().executeQuery("SELECT num FROM healthmember WHERE id = '" + id + "'");
+			rs = conn.createStatement().executeQuery("SELECT num FROM healthmember WHERE num = '" + num + "'");
 
 			if (rs.next()) {
 				pstmt = conn.prepareStatement("DELETE FROM healthmember WHERE num = ?");
@@ -228,7 +229,7 @@ class MemberManager {
 			rs = conn.createStatement().executeQuery("SELECT * FROM healthmember ORDER BY num ASC");
 
 			while (rs.next()) {
-				HealthMember member = new HealthMember();
+				HealthMember member = new HealthMember(false);
 				member.setNum(rs.getInt("num"));
 				member.setId(rs.getString("id"));
 				member.setName(rs.getString("name"));
@@ -252,10 +253,26 @@ class MemberManager {
 }
 
 class HealthMember {
+	static int count = 1;
 	private int num;
 	private String id;
 	private String name;
 	private String location;
+
+	public HealthMember(boolean isNew) {
+		if (isNew) {
+			this.num = count++;
+		}
+	}
+
+	public HealthMember(String id, String name, String location, boolean isNew) {
+		if (isNew) {
+			this.num = count++;
+		}
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
 
 	@Override
 	public String toString() {
@@ -300,5 +317,3 @@ class HealthMember {
 		this.location = location;
 	}
 }
-
-
